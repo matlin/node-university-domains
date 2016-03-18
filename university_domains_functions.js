43ef2288d2a2c58ee7051f8d34dd3dbc9a9a086f
Matthew Linkous <mlinkous@interapthq.com>
2016-03-18 13:56:22 -0400

    Added missing files

---
+++ university_domains_functions.js
@@ -0,0 +1,60 @@
+var data = require('university_domains.js');
+module.exports = {
+   find: find
+};
+
+var attributes = ['web_page', 'country', 'domain', 'name'];
+
+function printUsage(){
+   console.log("Usage: find('your_search_query', ['web_page' || 'country' || 'domain' || 'name']"); 
+}
+
+function find(query, attribute){
+   if (arguments.length > 2){
+      console.error("Too many paramters");
+      printUsage();
+      return;
+   }else{
+      if (attribute){
+         if(attributes.indexOf(attribute) === -1){
+            console.error("Invalid attribute");
+            printUsage();
+            return;
+         }
+         return attributeSeach(query);
+      }else{
+         if (query){
+            if (typeof query === "string"){
+               console.error("Search parameter needs to be a string.")
+               printUsage();
+               return;
+            }
+            return basicSearch(query);
+         }else{
+            return data;
+         }
+      }
+   }
+}
+
+function basicSearch(query){
+   var result = data.filter(function(item){
+      for (var i=0; i<attributes.length; i++){
+         if (item[attributes[i]].indexOf(query) >= 0){
+            return true;
+         }
+      }
+      return false;
+   });
+   return result;
+}
+
+function attributeSearch(query, attribute){
+   var result = data.filter(function(item){
+      if (item[attribute].indexOf(query) >= 0){
+         return true;
+      }
+      return false;
+   });
+   return result;
+}
